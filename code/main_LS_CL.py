fd5359d729461a39188fad9e44685bb1c6e1a9d4
Xuan-1998 <j503440616@berkeley.edu>
2023-08-12 14:59:40 -0700

    changing threshold

--- code/main_LS_CL.py
+++ code/main_LS_CL.py
@@ -81,7 +81,7 @@ def train(args):
                         n_steps=128,
                         n_epochs=10,
                         )
-        callback = CurriculumCallback(check_freq=1000, difficulty_increase_thresh=10.0)
+        callback = CurriculumCallback(check_freq=1000, difficulty_increase_thresh=0.01)
 
         model.learn(total_timesteps=args.num_steps, tb_log_name=f"ppo_lstm_difficulty_{difficulty_level}", callback=callback)
         # best_reward = -np.inf
